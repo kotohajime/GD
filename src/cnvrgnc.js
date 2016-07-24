10a513aff8abd599287df908cd4e6c4e85bb3254
kotohajime <toxictoxer@gmail.com>
2016-07-24 16:12:49 +0900

    make this constructor

--- src/cnvrgnc.js
+++ src/cnvrgnc.js
@@ -1,16 +1,2 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var c1 = require("./components/c1");
-var RootComponent = c1.component;
-
-console.log(c1.string);
-console.log(c1.childstring);
-console.log(c1.chichistring);
-console.log("git");
-
-
-ReactDOM.render(
-  <RootComponent />,
-  document.getElementById("main")
-);
-
