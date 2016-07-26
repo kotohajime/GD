12072b902339493aa0c74b87e7d138f44a0bad3a
kotohajime <toxictoxer@gmail.com>
2016-07-26 18:00:50 +0900

    a commit

--- src/components/c1.js
+++ src/components/c1.js
@@ -1,1 +1,11 @@
 var React = require("react");
+
+var PostidComponen = React.createClass({
+  render:function(){
+    return(
+      <div className="a-post">
+        
+      </div>
+    );
+  }
+});
